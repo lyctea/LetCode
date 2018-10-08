58ff2550dc817f35e18a140714f3384b6d7ff277
lyctea <396136337@qq.com>
2018-10-08 14:17:40 +0800

    <A> 709

---
+++ JavaScript/709.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function(str) {
+  // let result = '';
+  //
+  // if(!str) return result;
+  //
+  //
+  // for(const c of str) {
+  //   if(c.charCodeAt() >= 65 && c.charCodeAt() <= 90) {
+  //     result += String.fromCharCode(c.charCodeAt() + 32);
+  //   }else {
+  //     result += c;
+  //   }
+  // }
+  //
+  // return result;
+  
+  return str.split('').map((c) => {
+      return c >= 'A' && c <= 'Z' ? String.fromCharCode(c.charCodeAt() + 32) : c
+  }).join('');
+};
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 | 997 | [jewels-and-stones](https://github.com/lyctea/LetCode/blob/master/JavaScript/771.js) | Easy || ||
 | 807 | [Max Increase to Keep City Skyline](https://github.com/lyctea/LetCode/blob/master/JavaScript/807.js) | Easy || ||
 | 535 | [Encode and Decode TinyURL](https://github.com/lyctea/LetCode/blob/master/JavaScript/535.js) | Easy || ||
+| 709 | [To Lower Case](https://github.com/lyctea/LetCode/blob/master/JavaScript/709.js) | Easy || ||
