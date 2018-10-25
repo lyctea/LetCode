1774a25b7868c5882c8216516bfb9f205cf3c84d
lyctea <396136337@qq.com>
2018-10-25 12:43:15 +0800

    <A> 897

--- JavaScript/897.js
+++ JavaScript/897.js
@@ -9,11 +9,11 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var increasingBST = function(root) {
+var increasingBST = function (root) {
   let arr = [];
   // 中序遍历整棵树，将结果保存在数组中
-  (function inorder(root){
-    if(root===null) return ;
+  (function inorder (root) {
+    if (root === null) return;
     inorder(root.left);
     arr.push(root.val);
     inorder(root.right);
@@ -22,9 +22,9 @@ var increasingBST = function(root) {
   let res = new TreeNode();
   let tmp = res;
   // 遍历遍历后的数组，创建的信息的二叉树
-  for(let i=0; i<arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     tmp.val = arr[i];
-    if(i<arr.length-1){
+    if (i < arr.length - 1) {
       tmp.right = new TreeNode();
       tmp = tmp.right;
     }
